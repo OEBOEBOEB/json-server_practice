65279fe62e7424e3850549e524c022102adbfab6
unknown <a1043305@mail.nuk.edu.tw>
2019-07-14 02:27:08 +0800

    upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// var data = "title=aa";
+
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+//     if (this.readyState === 4) {
+//         console.log(this.responseText);
+//     }
+// });
+
+// xhr.open("POST", "http://localhost:3000/posts");
+// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// xhr.setRequestHeader("cache-control", "no-cache");
+// xhr.setRequestHeader("Postman-Token", "4c996eea-ceb2-4a48-b3e6-c326b99ca109");
+
+// xhr.send(data);
